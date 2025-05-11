27eeac026f8fc883d6874209ab3e7883eec29db1
Ngangam Haolai <ngangamhaolai100@gmail.com>
2025-05-11 14:30:53 +0530

    attempt to fix DB not connecting 1.0

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -7,7 +7,7 @@ const connectDB = async()=>
 {
     try
     {
-        await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/BookMyFit");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("connected to Database successfully.");
     }
     catch(err)
